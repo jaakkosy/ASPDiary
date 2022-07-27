a5d0d2a9e2dfe4e751a0490cf239a69fb808ee5f
jaakkosy <jaakkosyrjamaki92@hotmail.com>
2022-07-27 13:14:30 +0300

    first commit

---
+++ DiaryWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DiaryWebApp.Models;
+
+namespace DiaryWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DiaryWebApp.Models.Topic> Topic { get; set; }
+    }
+}
