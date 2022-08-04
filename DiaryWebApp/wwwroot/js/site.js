d47624d1d0b0a234cad1e62611b4fe92f77a57be
jaakkosy <jaakkosyrjamaki92@hotmail.com>
2022-08-04 14:00:55 +0300

    added js

--- DiaryWebApp/wwwroot/js/site.js
+++ DiaryWebApp/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var a;
+function show_hide() {
+    if (a === 1) {
+        document.getElementById("instructions").style.display = "inline";
+        return a = 0;
+    } else {
+        document.getElementById("instructions").style.display = "none";
+        return a = 1;
+    }
+}
